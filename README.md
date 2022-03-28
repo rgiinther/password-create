65d8778a8dc0a9e27403d9d128877df80435374c
rgiinther <r.giin95@gmail.com>
2022-03-27 21:31:09 -0500

    added JS code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # password-create
+
+This site is a random password generator. 
+You can use the slider to pick the desired length of password. 
--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-// Assignment code here
+//generate random password
+function generate(){
 
+  //set password length/complexity
+  let complexity = document.getElementById("slider").value;
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  //possible password values
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = "";
 
-  passwordText.value = password;
+  //create for loop to choose password characters
+  for(var i = 0; i <= complexity; i++){
+      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+
+  //add password to textbox/display area
+  document.getElementById("password").value = password;
+
+  //add password to previously generated passwords section
+  document.getElementById("lastNums").innerHTML += password + "<br />";
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//set default length display of 64
+document.getElementById("length").innerHTML = "Length: 64";
+
+//function to set length based on slider position
+document.getElementById("slider").oninput = function(){
+
+  if(document.getElementById("slider").value > 0){
+      document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+  }
+  else{
+      document.getElementById("length").innerHTML = "Length: 1";
+  }
+
+}
